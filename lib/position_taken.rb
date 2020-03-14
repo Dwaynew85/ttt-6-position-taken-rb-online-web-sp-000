147acb854013df3387582b160cdf6699994b2036
Dwayne Weller <mr_dwayne_w@yahoo.com>
2020-03-14 05:08:13 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,6 +1,6 @@
 def position_taken?(board, index_number)
   !(board[index_number] ==  " " || board[index_number] == "")
-  if index_number > 8
-    "nil"
   end
 end
+
+board = [" ", " ", "0", "", " ", "", " ", "", "X"]
